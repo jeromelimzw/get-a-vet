969c683723d867069c7176090f5b42f18e79620a
jeromelimzw <jerome.lim.zw@gmail.com>
2019-05-01 17:18:24 +0800

    Add Location List Page

---
+++ src/LocationList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LocationList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>LocationList</h1>;
+  }
+}
+
+export default LocationList;
